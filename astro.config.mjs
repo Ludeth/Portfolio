49ab6c5cbe46636a01cfcb31a9b6f297cebe497b
Davide Di Marsilio <49905178+Ludeth@users.noreply.github.com>
2023-07-05 17:46:08 +0200

    Added localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
 
 export default defineConfig({
-  site: 'https://ludeth.github.io'
-})
+  site: 'https://ludeth.github.io',
+  integrations: [astroI18next()],
+});
