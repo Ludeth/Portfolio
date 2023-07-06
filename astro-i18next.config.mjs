f7a8c5d84ba9bd1ba91da408c7759bdc7cf8f134
Davide Di Marsilio <49905178+Ludeth@users.noreply.github.com>
2023-07-06 21:23:03 +0200

    Fixed site link

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,5 +1,37 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
+  defaultLocale: "en",
+  locales: ["en", "it"],
+};
+
+/*
+import en from "./public/locales/en/translation.json" assert {type: "json"};
+import it from "./public/locales/it/translation.json" assert {type: "json"};
+
+export default {
     defaultLocale: "en",
     locales: ["en", "it"],
+    fallbackLng : "en",
+    i18nextServer: {
+      debug: true,
+      resources: {
+        en: {
+          translation: {
+              ...en
+          }
+        },
+        it: {
+          translation: {
+              ...it
+          }
+        }
+      },
+      initImmediate: false,
+      preload: true,
+      backend: null
+    },
+    i18nextServerPlugins: {
+      fsBackend: null
+    }
   };
+  */
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 
 export default defineConfig({
-  site: 'https://ludeth.github.io',
+  site: 'https://dimarsil.io/',
   integrations: [astroI18next()],
 });
