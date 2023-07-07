cc968b18c0052d89374fc320ff1fe89045d41fd9
Davide Di Marsilio <49905178+Ludeth@users.noreply.github.com>
2023-07-07 09:59:10 +0200

    Fixed something
    Still unable to make astro::content work

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
-	work: defineCollection({
+	work_en: defineCollection({
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+	work_it: defineCollection({
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
