82096169b367dbd0d100e39ab3b2e0ef6c0e7467
Davide Di Marsilio <49905178+Ludeth@users.noreply.github.com>
2023-07-14 19:38:05 +0200

    Fixed 404 pages

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -4,7 +4,7 @@ export default {
   locales: ["en", "it"],
 };
 
- /*//I copied this because on win11 there's a bug where the locales won't load propely while previewing the page
+/*//I copied this because on win11 there's a bug where the locales won't load propely while previewing the page
 import en from "./public/locales/en/translation.json" assert {type: "json"};
 import it from "./public/locales/it/translation.json" assert {type: "json"};
 
