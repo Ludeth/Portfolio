a1ffead99831a25dc836d2d1df7aa3283332f4e6
Davide Di Marsilio <49905178+Ludeth@users.noreply.github.com>
2024-07-24 18:55:57 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import astroI18next from "astro-i18next";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://dimarsil.io/',
+  site: 'https://dimarsilio.it/',
   integrations: [astroI18next(),sitemap({
     i18n: {
       defaultLocale: 'en',
